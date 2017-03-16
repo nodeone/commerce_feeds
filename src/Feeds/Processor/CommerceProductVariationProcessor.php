6936eb87ec785a6ce56d7b9fe26cfecbd26a0a86
Anatoly <politsin@gmail.com>
2017-03-16 16:46:48 +0300

    Update CommerceProductVariationProcessor.php

--- src/Feeds/Processor/CommerceProductVariationProcessor.php
+++ src/Feeds/Processor/CommerceProductVariationProcessor.php
@@ -1,8 +1,8 @@
 <?php
 
 namespace Drupal\commerce_feeds\Feeds\Processor;
-use Drupal\feeds\Feeds\Processor\EntityProcessorBase;
 
+use Drupal\feeds\Feeds\Processor\EntityProcessorBase;
 
 /**
  * Defines a user processor.
@@ -14,7 +14,7 @@ use Drupal\feeds\Feeds\Processor\EntityProcessorBase;
  *   title = @Translation("Product variation"),
  *   description = @Translation("Product variations."),
  *   entity_type = "commerce_product_variation",
- *   arguments = {"@entity.manager", "@entity.query"},
+ *   arguments = {"@entity.manager", "@entity.query", "@entity_type.bundle.info"},
  *   form = {
  *     "configuration" = "Drupal\feeds\Feeds\Processor\Form\DefaultEntityProcessorForm",
  *     "option" = "Drupal\feeds\Feeds\Processor\Form\EntityProcessorOptionForm",
