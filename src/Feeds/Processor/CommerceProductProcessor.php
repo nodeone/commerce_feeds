e345c61a20b02a47b9ccfa4432cb96d640b0b011
Anatoly <politsin@gmail.com>
2017-03-16 16:46:15 +0300

    Update CommerceProductProcessor.php

--- src/Feeds/Processor/CommerceProductProcessor.php
+++ src/Feeds/Processor/CommerceProductProcessor.php
@@ -1,8 +1,8 @@
 <?php
 
 namespace Drupal\commerce_feeds\Feeds\Processor;
-use Drupal\feeds\Feeds\Processor\EntityProcessorBase;
 
+use Drupal\feeds\Feeds\Processor\EntityProcessorBase;
 
 /**
  * Defines a user processor.
@@ -14,7 +14,7 @@ use Drupal\feeds\Feeds\Processor\EntityProcessorBase;
  *   title = @Translation("Product"),
  *   description = @Translation("Product."),
  *   entity_type = "commerce_product",
- *   arguments = {"@entity.manager", "@entity.query"},
+ *   arguments = {"@entity.manager", "@entity.query", "@entity_type.bundle.info"},
  *   form = {
  *     "configuration" = "Drupal\feeds\Feeds\Processor\Form\DefaultEntityProcessorForm",
  *     "option" = "Drupal\feeds\Feeds\Processor\Form\EntityProcessorOptionForm",
